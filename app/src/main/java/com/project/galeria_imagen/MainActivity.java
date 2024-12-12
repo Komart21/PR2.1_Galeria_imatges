6b8bec0978bacc32f9108f8c236ca6455ea8729d
Komart21 <152431488+Komart21@users.noreply.github.com>
2024-12-12 17:09:18 +0100

    hecho full-size

--- app/src/main/java/com/project/galeria_imagen/MainActivity.java
+++ app/src/main/java/com/project/galeria_imagen/MainActivity.java
@@ -2,67 +2,116 @@ package com.project.galeria_imagen;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int RC_PHOTO_PICKER = 0;
+    private Uri photoURI;
+    private ActivityResultLauncher<Intent> imagePickerLauncher;
+    private ActivityResultLauncher<Intent> cameraFullSizeLauncher;
+    private ActivityResultLauncher<Intent> cameraThumbnailLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+
+        ImageView imageView = findViewById(R.id.img);
+
+        // Configurar lanzadores de actividad
+        imagePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Uri uri = result.getData().getData();
+                        imageView.setImageURI(uri);
+                    }
+                });
+
+        cameraFullSizeLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
-                            // There are no request codes
-                            Intent data = result.getData();
-                            Uri uri = data.getData();
                             ImageView imageView = findViewById(R.id.img);
-                            imageView.setImageURI(uri);
+                            if (photoURI != null) {
+                                imageView.setImageURI(photoURI);
+                            } else {
+                                Log.e("ERROR", "No photoURI available");
+                            }
                         }
                     }
                 });
 
-        Button buton = findViewById(R.id.button);
-        buton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Create Intent
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/jpg");
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                //Launch activity to get result
-                someActivityResultLauncher.launch(intent);
+        cameraThumbnailLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Bitmap thumbnail = (Bitmap) result.getData().getExtras().get("data");
+                        imageView.setImageBitmap(thumbnail);
+                    }
+                });
+
+        // Configurar botones
+        Button selectImageButton = findViewById(R.id.buttonGallery);
+        selectImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            imagePickerLauncher.launch(intent);
+        });
+
+        Button photoButton = findViewById(R.id.buttonFullSize);
+        photoButton.setOnClickListener(view -> {
+            try {
+                File photoFile = createImageFile();
+                if (photoFile != null) {
+                    photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            "com.project.galeria_imagen.fileprovider",  // Cambiar esta línea
+                            photoFile);
+
+                    Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoURI);
+                    cameraFullSizeLauncher.launch(intent);
+                }
+            } catch (IOException e) {
+                Log.e("ERROR", "Error creating file", e);
             }
         });
 
-
+        Button thumbnailPhotoButton = findViewById(R.id.buttonThumbnail);
+        thumbnailPhotoButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraThumbnailLauncher.launch(intent);
+        });
     }
 
-
-
+    private File createImageFile() throws IOException {
+        // Crear un nombre único para el archivo de imagen
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
 }
+
